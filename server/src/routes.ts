643bc2ce39764f7f6f4a624ef4c89a409922413a
gustavosalviato <gustavosalviato8@gmail.com>
2023-01-21 11:21:07 -0300

    Rota para habilitar | desabilitar o hábito

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -77,6 +77,80 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     }
   })
+
+  app.patch('/habits/:id/toggle', async (req, res) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(req.params)
+
+    let today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabitAlreadyCompleted = await prisma.dayHabit.findUnique({
+      where: {
+        dayId_habitId: {
+          dayId: day.id,
+          habitId: id,
+        }
+      }
+    })
+
+    if (dayHabitAlreadyCompleted) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabitAlreadyCompleted.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          habitId: id,
+          dayId: day.id
+        }
+      })
+    }
+
+
+  })
+
+  app.get('/summary', async (req, res) => {
+    const summary = await prisma.$queryRaw`
+      SELECT
+       D.id,
+       D.date, 
+       (
+        SELECT cast(count(*) as float)
+        FROM day_habits DH
+        WHERE DH.dayId = D.id
+       ) as completed,
+       (
+        SELECT cast(count(*) as float)
+        FROM habit_week_days HWD
+        JOIN habits H 
+          ON H.id = HWD.habitId
+        WHERE 
+          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+       ) as amount
+      FROM days D
+    `
+    return summary
+  })
 }
 
 
